b8855e0cf5df1ae0f3d5bab1adcf7a9d671a8905
TristanSch1 <tristan.sch57@gmail.com>
2022-03-25 22:11:20 +0100

    ✨ Added mongoDB connexion using Mongoose

--- server/app.js
+++ server/app.js
@@ -3,6 +3,19 @@ const path = require("path");
 
 const app = express();
 
+//Utilisation de mongoose pour se connecter à MongoDB
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+async function main() {
+  mongoose
+    .connect(
+      "mongodb+srv://natix:Mttz4fr5jrjC9Q8@genshinproject.jrr5l.mongodb.net/genshinProject?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
+}
 //Configuration de express
 const distDir = "../src/";
 app.use("/pages", express.static(path.join(__dirname, distDir, "/pages")));
