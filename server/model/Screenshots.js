10a2d892e87cacc8ac16373597d130907870d67f
TristanSch1 <tristan.sch57@gmail.com>
2022-03-26 18:34:01 +0100

    Added the form popup in the screenshot library and the database model for the screenshots

---
+++ server/model/Screenshots.js
@@ -0,0 +1,19 @@
+/*
+  Création du schéma de données pour les screenshots
+  avec trois champs, un auteur, une description et 
+  l'url de l'image.
+*/
+
+const mongoose = require("mongoose");
+
+const screenshotsSchema = mongoose.Schema({
+  author: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+});
+
+/*
+  Exportation du schéma en tant que modèle Mongoose,le 
+  rendant disponible dans toute l'application Express.
+*/
+module.exports = mongoose.model("Screenshots", screenshotsSchema);
---
+++ src/assets/script/library.js
@@ -0,0 +1,30 @@
+const libraryContent = document.querySelector("#libraryContent");
+const addScreenshotButton = document.querySelector("#libraryContent button");
+const addScreenshotPopup = document.querySelector("#addScreenshotPopup");
+const closePopup = document.querySelector("#closePopup");
+const author = document.querySelector("#author");
+const description = document.querySelector("#description");
+const imageUrl = document.querySelector("#imageUrl");
+const authorPreview = document.querySelector("#authorPreview #content");
+const descriptionPreview = document.querySelector(
+  "#descriptionPreview #content"
+);
+//Open form popup
+
+addScreenshotButton.addEventListener("click", () => {
+  addScreenshotPopup.style.display = "flex";
+});
+
+//Close form popup
+
+closePopup.addEventListener("click", () => {
+  addScreenshotPopup.style.display = "none";
+});
+
+author.addEventListener("input", (content) => {
+  authorPreview.innerText = content.target.value;
+});
+
+description.addEventListener("input", (content) => {
+  descriptionPreview.innerText = content.target.value;
+});
